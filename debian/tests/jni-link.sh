4ce759d0d6b9a752603d1b182e63c90e30074ead
Matthias Klose <doko@ubuntu.com>
2024-04-24 15:11:44 +0200

    * Add autopkgtest to test that JNI applications using the invocation API     can be linked.

---
+++ debian/tests/jni-link.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
+
+host_arch="${DEB_HOST_ARCH:-$(dpkg --print-architecture)}"
+jdk_path=$(echo /usr/lib/jvm/java-22-openjdk-amd64 | sed "s/-[^-]*$/-$host_arch/")
+
+cat <<EOF > ${AUTOPKGTEST_TMP}/test.cpp
+#include <jni.h>
+
+int main(){
+    JavaVM *jvm;
+    JNIEnv *env;
+    JavaVMInitArgs vm_args;
+    vm_args.version = JNI_VERSION_1_8;
+    vm_args.nOptions = 0;
+    vm_args.ignoreUnrecognized = false;
+    JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args);
+}
+EOF
+
+find ${jdk_path}/lib -name libjvm.so | while read -r x; do
+    jvmdir="$(dirname ${x})"
+    echo ${jvmdir}
+    g++ ${AUTOPKGTEST_TMP}/test.cpp \
+        -I${jdk_path}/include -I${jdk_path}/include/linux \
+        -L${jvmdir} -ljvm
+    LD_LIBRARY_PATH=${jvmdir} ./a.out && echo "${jvmdir} - vm created"
+done
